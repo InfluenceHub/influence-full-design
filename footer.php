fd7f11c54f76a42b059966c60d3982660dbd0850
Matt Maguire <mhmaguire0@gmail.com>
2014-08-05 07:04:12 -0700

    blow up fonts a little bit for readability and reformat footer links and copy

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 		<!-- Footer -->
 		<footer id="footer" style="background-color= #F4F4F4;">
 			<div class="shell">
+				<div class='foot-copy'>
 				<?php $copyright_text = carbon_get_theme_option('inf_copyright_text');
 
 				if(!empty($copyright_text)) { ?>
         <img style="display: inline-block; vertical-align: middle;" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/footer-in.png" width="30" height="30" alt="In Logo" />
 				<p class="copy"><?php echo str_replace('{year}', date('Y'), $copyright_text); ?></p>
+			</div>
 				<?php
         }
         wp_nav_menu( array(
