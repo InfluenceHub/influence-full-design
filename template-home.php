b7ec7141f3925a1ec5b1154bab8bd56bb6044e8d
Cameron Banowsky <cameron@code.cd>
2014-08-11 14:31:05 -0700

    test

--- template-home.php
+++ template-home.php
@@ -7,11 +7,6 @@ get_header();
 the_post();
 //Detect mobile
 ?>
-<?php 
- $image_oneID = carbon_get_post_meta($list_item->ID, 'hslide_image');
- $image_oneA = wp_get_attachment_image_src($image_oneID,'full', true);
- $image_one = $image_oneA[0];
- ?>
       <section id="content">
         <div class="content_wrapper top-block">
       <div class="column-two">
@@ -95,7 +90,6 @@ the_post();
         </div>
       </div>
     </div>
-
     <div class="ad_wrapper-block">
       <?php inf_footer_signup() ?>
       <?php //inf_browse_by(); ?>
