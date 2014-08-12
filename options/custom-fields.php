7ecd144b31f9a632d896ef92bfc8f953b1527d34
Cameron Banowsky <cameron@code.cd>
2014-08-12 08:53:56 -0700

    add featured images

--- options/custom-fields.php
+++ options/custom-fields.php
@@ -162,6 +162,14 @@ Carbon_Container::factory('custom_fields', __('Additional Featured Images', 'inf
 				Carbon_Field::factory('attachment', 'inf_featured_image', 'Image')
 			))
 	));
+	Carbon_Container::factory('custom_fields', __('Additional Featured Images', 'inf'))
+	->show_on_post_type(array('post', 'inf_market'))
+	->add_fields(array(
+		Carbon_Field::factory('complex', 'inf_featured_images', 'Images')
+			->add_fields(array(
+				Carbon_Field::factory('attachment', 'inf_featured_image', 'Image')
+			))
+	));
 
 Carbon_Container::factory('term_meta', __('Category settings', 'inf'))
 	->show_on_taxonomy('category')
