34ce2ad65e860ac2c219a9d8153ffd9c540661a8
Cameron Banowsky <cameron@code.cd>
2014-08-12 09:25:03 -0700

    add shit

--- options/custom-fields.php
+++ options/custom-fields.php
@@ -273,15 +273,13 @@ Carbon_Container::factory('custom_fields', __('Slide Options', 'inf'))
 		Carbon_Field::factory('text', 'homebox1_title_two', 'Secondary Title'),
 		Carbon_Field::factory('text', 'homebox1_link_url', 'Link URL'),
 		Carbon_Field::factory('attachment', 'homebox_image1', 'Image')
-			->help_text('dimensions 300 x 200 pixels')
 	)); 
-  Carbon_Container::factory('custom_fields', __('Box Options', 'inf'))
+  Carbon_Container::factory('custom_fields', __('Box Options2', 'inf'))
 	->show_on_post_type('inf_home_box2')
   ->add_fields(array(
 		Carbon_Field::factory('text', 'homebox2_title_two', 'Secondary Title'),
 		Carbon_Field::factory('text', 'homebox2_link_url', 'Link URL'),
 		Carbon_Field::factory('attachment', 'homebox_image2', 'Image')
-			->help_text('dimensions 300 x 200 pixels')
 	)); 
 /*
 Carbon_Container::factory('custom_fields', __('Featured Theme Two', 'inf'))
