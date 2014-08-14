6f9d15ffc6c194ac674de87c9eec910a2d5075b0
Cameron Banowsky <cameron@code.cd>
2014-08-14 15:05:17 -0700

    testing

--- functions_two.php
+++ functions_two.php
@@ -428,9 +428,10 @@
                         </div> <!-- END .home-feed-post-info -->
                   </li>
 
-                 <img width="600px" height="59px" src="<?php echo get_stylesheet_directory_uri(); ?>/images/breaker.png" /> -->
 
                 <?php elseif ($k == 7 && !is_null($latest_theme)): // replace the last one with a featured theme, if it exists ?>
+                   <img width="600px" height="59px" src="<?php echo get_stylesheet_directory_uri(); ?>/images/breaker.png" />
+
                   <?php $post = $latest_theme ?>
 
                   <li class="column home-feed-post home-feed-side-by-side">
