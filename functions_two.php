4f50c1aaadfc7c5414d565aa74046831e48868e2
Cameron Banowsky <cameron@code.cd>
2014-08-14 11:25:54 -0700

    overlay margin

--- functions_two.php
+++ functions_two.php
@@ -146,7 +146,7 @@
         <li>
           <div class="image-wrap">
             <div class="main">
-              <img class="look-overlay" style="margin-left: 25px;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/the_look_overlay.png" />
+              <img class="look-overlay" style="margin-left: 35px;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/the_look_overlay.png" />
               <a href="<?php echo $slide_link; ?>" title="<?php echo $longTitle; ?>">
               <?php
               if (trim($image_one) > '') {
