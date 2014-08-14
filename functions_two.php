0f56252b7a6f9468cf67319957b39dfff8c866e8
Cameron Banowsky <cameron@code.cd>
2014-08-14 11:43:09 -0700

    breaker fix?

--- functions_two.php
+++ functions_two.php
@@ -428,7 +428,9 @@
                         </div> <!-- END .home-feed-post-info -->
                         <br />
                   </li>
-                <div class="breaker-shell"></div>
+                <div class="breaker-shell">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/breaker.png" />
+                </div>
                 <?php elseif ($k == 7 && !is_null($latest_theme)): // replace the last one with a featured theme, if it exists ?>
                   <?php $post = $latest_theme ?>
                   <li class="column home-feed-post home-feed-side-by-side">
