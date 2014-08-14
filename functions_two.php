0952ca60556e7b5395898e444229950f81543027
Cameron Banowsky <cameron@code.cd>
2014-08-14 15:19:36 -0700

    testing

--- functions_two.php
+++ functions_two.php
@@ -427,9 +427,10 @@
                             
                         </div> <!-- END .home-feed-post-info -->
                   </li>
-                <?php endforeach; ?>
-                 <img width="600px" height="59px" src='<?php echo get_stylesheet_directory_uri(); ?>/images/breaker.png' />                
 
+<!--                 <img width="600px" height="59px" src='<?php echo get_stylesheet_directory_uri(); ?>/images/breaker.png' />                
+
+ -->
                 <?php elseif ($k == 7 && !is_null($latest_theme)): // replace the last one with a featured theme, if it exists ?>
 
                   <?php $post = $latest_theme ?>
