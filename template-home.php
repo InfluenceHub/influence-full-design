5d0918e50672f3da2db8c2673f0f71d222bebd77
Cameron Banowsky <cameron@code.cd>
2014-08-18 16:20:00 -0700

    style changes

--- template-home.php
+++ template-home.php
@@ -9,7 +9,7 @@ the_post();
 ?>
       <section id="content">
         <div class="top-block">
-      <div class="column-two no-mobile" style="margin-left: 25px;">
+      <div class="column-two no-mobile" style="margin-left: 15px; margin-right: -15px;">
         <a class="left-arrow no-mobile" style="z-index: 1;"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/left-arrow.png"></a>
         <a class="right-arrow no-mobile" style="z-index: 1;"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/right-arrow.png"></a>
         <?php inf_home_topslider()?>
