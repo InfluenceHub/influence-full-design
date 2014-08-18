4464a2de9c805327282cc8a6803f8210d8dd09e4
Cameron Banowsky <cameron@code.cd>
2014-08-18 12:02:36 -0700

    theme archive creation

--- theme-archive.php
+++ theme-archive.php
@@ -34,14 +34,13 @@ the_post();
             
             foreach ($themes as $k => $post) {
                 setup_postdata($post);
-                $attachments = new Attachments('inf_featured_image', $post->ID);
+                $attachments = new Attachments('inf_featured_images', $post->ID);
               ?>
 
                 <div class="column-three">
                     <div class="column-left int-archive-column-left">
                       <a href="<?php the_permalink() ?>" class="int-archive-title-link">
                         <h3 style="font-family: BaskervilleBT-Italic; font-weight: 300; text-transform: none; font-size: 30px;"><?php the_title(); ?></h3>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/int_more_link.png" />
                       </a>
                     </div>
                     <div class="column-right int-archive-column-right">
@@ -50,7 +49,7 @@ the_post();
                           if($attachments->exist()){
                             $first_attachment = $attachments->get_single(0);
 
-                            $image = wp_get_attachment_image_src($first_attachment->id, 'inf_featured_image');
+                            $image = wp_get_attachment_image_src($first_attachment->id, 'inf_featured_images');
 
                             echo '<img src="'.$image[0].'" style="width:100%">';
 
