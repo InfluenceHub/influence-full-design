a93fe007b6456ccc4e7e15eb8fb02c8730ae6251
Cameron Banowsky <cameron@code.cd>
2014-08-18 15:09:56 -0700

    style changes

--- theme-archive.php
+++ theme-archive.php
@@ -37,9 +37,12 @@ the_post();
                 <div class="column-three">
                     <div class="theme-archive-column-left">
                     <a href="<?php the_permalink() ?>" class="theme-archive-image-link">
+                      <div class="theme-image-sizer">
+
                           <?php if (has_post_thumbnail()) {
                               the_post_thumbnail('inf_featured_theme');
                           } ?>  
+                       </div>
                         <h3 style="font-family: BaskervilleBT-Italic; font-weight: 300; text-transform: none; font-size: 30px;"><?php the_title(); ?></h3>
                       </a>
                     </div>
