10434aa71f354ea577458abb759d9d58c468d428
Cameron Banowsky <cameron@code.cd>
2014-08-18 16:45:43 -0700

    style changes

--- template-home.php
+++ template-home.php
@@ -24,7 +24,7 @@ the_post();
           </a>
         </div>
         <div style="margin-top: -30px;">
-          <a href="<?php echo carbon_get_post_meta($box1->ID, 'homebox1_link_url'); ?>" class="cta-view-more">
+          <a href="<?php echo carbon_get_post_meta($box1->ID, 'homebox1_link_url'); ?>">
             <img height="43px" width="134px" src="<?php echo get_stylesheet_directory_uri(); ?>/images/cta.png" />
           </a>
           </div>
@@ -37,7 +37,7 @@ the_post();
           </a>
         </div>
         <div style="margin-top: -30px;">        
-          <a href="<?php echo carbon_get_post_meta($box2->ID, 'homebox2_link_url'); ?>" class="cta-view-more">
+          <a href="<?php echo carbon_get_post_meta($box2->ID, 'homebox2_link_url'); ?>">
             <img height="43px" width="134px" src="<?php echo get_stylesheet_directory_uri(); ?>/images/cta.png" />
           </a>
           </div>
