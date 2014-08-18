d769563459fb2fb35c748b757deda4abe0e22266
Cameron Banowsky <cameron@code.cd>
2014-08-18 13:25:33 -0700

    style changes

--- theme-archive.php
+++ theme-archive.php
@@ -39,7 +39,7 @@ the_post();
 
                 <div class="column-three">
                     <div class="theme-archive-column-left">
-                    <a href="<?php the_permalink() ?>" class="theme-archive-image-link">
+                    <a href="<?php the_permalink() ?>" class="theme-archive-image-link theme-archive-image-link-overlay">
                           <?php if (has_post_thumbnail()) {
                               the_post_thumbnail('inf_featured_theme');
                           } ?>  
