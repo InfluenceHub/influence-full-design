40fbfbf261f32bb6d137d510b4c070a6bd788342
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-08-19 09:58:37 -0700

    fix white space

--- template-influencers.php
+++ template-influencers.php
@@ -81,7 +81,7 @@ get_header();
         <!-- BEGIN IFRAME TAG - theinfluence 300x600 < - DO NOT MODIFY -->
         <!-- <IFRAME SRC="http://ib.adnxs.com/tt?id=2438106&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="600"></IFRAME> 
         <?php inf_item_of_the_day(); ?> 
-        <a href="<?php echo home_url(); ?>/my-influence" class="subscribe-img"></a> -->
+        <!--<a href="<?php echo home_url(); ?>/my-influence" class="subscribe-img"></a> -->
       </div>
     </div>
     <div class="sorted-content influencers-az">
