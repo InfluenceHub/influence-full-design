8832419d6d620b5281feb70ebb21eb1311b722dd
Cameron Banowsky <cameron@code.cd>
2014-08-22 14:09:07 -0700

    submenu fix

--- js/functions.js
+++ js/functions.js
@@ -21,11 +21,11 @@ jQuery(function($) {
     var pages = {
       "/in-focus/":"/in-focus/"
     }
-  //$("#menu-main-menu li a").on('click', function(e) {
+  $("#menu-item-140 a").on('click', function(e) {
     //pages we want to redirect on mobile and where to redirect to
-   // var pages = {
-     // "/influencers/":"/influencers/"
-    //}
+    var pages = {
+      "/influencers/":"/influencers/"
+    }
     
     if (pages[this.pathname] && isMobile.matches) { 
       e.preventDefault();
