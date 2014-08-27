baf8737349873d0a9bc2e4614e19df39e2357704
Cameron Banowsky <cameron@code.cd>
2014-08-27 12:15:53 -0700

    change the signup bar to 80% bleed

--- template-home.php
+++ template-home.php
@@ -9,9 +9,9 @@ the_post();
 ?>
 
       <section id="content">
+        <?php inf_footer_signup() ?>
 
         <div class="top-block">
-        <?php inf_footer_signup() ?>
 
       <div class="column-two no-mobile" style="margin-left: 15px; margin-right: -15px;">
         <a class="left-arrow no-mobile" style="z-index: 1;"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/left-arrow.png"></a>
