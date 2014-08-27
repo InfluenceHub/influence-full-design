36fbeee861cae4aaa0851c3ab56df7402583f114
Cameron Banowsky <cameron@code.cd>
2014-08-27 14:01:29 -0700

    return interview page to slide show

--- single-inf-interview.php
+++ single-inf-interview.php
@@ -24,7 +24,7 @@
 <br class="no-mobile" /><br class="no-mobile" /><br class="no-mobile" />
 
         </div>
-        <div id="topSlider" class="interview-slider group" style="margin-left: -10px; overflow: visible;">
+        <div id="topSlider" class="interview-slider group" style="overflow: visible;">
           <ul class="slides" >
           <?php
             $attachments = new Attachments('inf_interview_attachments', $post->ID);
