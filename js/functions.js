901461fe93b0078ff9a4d8f9dc4a17986afd520f
Cameron Banowsky <cameron@code.cd>
2014-08-27 12:32:16 -0700

    change the signup bar to 80% bleed

--- js/functions.js
+++ js/functions.js
@@ -662,6 +662,25 @@ var headerHeight = parseInt($('#header').height()+20);
     $('#content').css({'margin-top': headerHeight+'px'});
   }
 
+//Change Opacity on Scroll
+var signupHeight = parseInt($('.browse').height()+20);
+  if ($('#top_ad-block').length > 0) {
+    var topOffset = $('#top_ad-block').height();
+    $(window).scroll(function() {
+      var thisEl = $('.browse');
+      if ($(window).scrollTop() >= topOffset && !thisEl.hasClass('pinned')) {
+        thisEl.addClass('pinned');
+        $('#content').css({'margin-top': headerHeight+'px'});
+      } else if ($(window).scrollTop() < topOffset && thisEl.hasClass('pinned')) {
+        thisEl.removeClass('pinned');
+        $('#content').css({'margin-top': '0px'});
+      }
+    });
+  } else {
+    $('.browse').addClass('pinned');
+    $('#content').css({'margin-top': headerHeight+'px'});
+  }
+
   
 //Initialize Home Page Slider
   function homeSliderInit() {
