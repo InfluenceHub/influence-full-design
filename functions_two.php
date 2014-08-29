917bb506ac19b56599c6ff1cc5078ad6d1b4d0ec
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-08-29 15:51:45 -0700

    more testing

--- functions_two.php
+++ functions_two.php
@@ -85,31 +85,30 @@ $args = array(
     <?php echo get_the_title($postID); ?>
   </div>
   <div class="favorites" style="border-left: 10px solid #d5f7ec;">
-   <a href="<?php echo $link1; ?>" class="favorites-link-overlay">
-    Shop Item
+   <a href="<?php echo $link1; ?>">
     <img src="<?php echo $image1SRC; ?>" width="133px" height="133px" />
     </a>
   </div>
   <div class="favorites">
-   <a href="<?php echo $link2; ?>" class="favorites-link-overlay">
+   <a href="<?php echo $link2; ?>">
    Shop Item
     <img src="<?php echo $image2SRC; ?>" width="133px" height="133px" />
     </a>
   </div>
   <div class="favorites">
-   <a href="<?php echo $link3; ?>" class="favorites-link-overlay">
+   <a href="<?php echo $link3; ?>">
    Shop Item
     <img src="<?php echo $image3SRC; ?>" width="133px" height="133px" />
     </a>
   </div>
   <div class="favorites">
-   <a href="<?php echo $link4; ?>" class="favorites-link-overlay">
+   <a href="<?php echo $link4; ?>">
    Shop Item
     <img src="<?php echo $image4SRC; ?>" width="133px" height="133px" />
     </a>
   </div>
   <div class="favorites">
-   <a href="<?php echo $link5; ?>" class="favorites-link-overlay">
+   <a href="<?php echo $link5; ?>">
    Shop Item
     <img src="<?php echo $image5SRC; ?>" width="133px" height="133px"/>
     </a>
