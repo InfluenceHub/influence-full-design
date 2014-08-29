85518cb9625bea9dfba8a27fc12dcae3707a70de
pixalee <aeli@pixalee.com>
2014-08-29 16:01:36 -0700

    Update single-inf_theme.php

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -60,12 +60,14 @@
 
 						//$exact_items = carbon_get_the_post_meta('inf_influencer_products'); 
             $products_sections = carbon_get_the_post_meta('inf_post_products_sections', 'complex');
+			global $wp;
+			$current_url = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
             ?>
             <div class="shop-main-theme">
 				<div class="shop-main-title"><?php echo get_the_title(get_the_ID()); ?></div>
 						<div class="text-holder" style="margin:0; padding:0; height:auto;">
-              <img class="shop-published-by1" style="display:block; margin:20px auto 40px;" src="<?php bloginfo('stylesheet_directory'); ?>/images/published-by-danielle.png" />
-			  <div class="shop-main-social"></div>
+              <img class="shop-published-by1" style="display:block; margin:20px auto 30px;" src="<?php bloginfo('stylesheet_directory'); ?>/images/published-by-danielle.png" />
+			  <div class="shop-main-social"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo ($current_url) ?>&redirect_uri=<?php echo urlencode($current_url) ?>" class="facebook"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social_fb.jpg" /></a> <a href="https://instagram.com/theinfluence" class="instagram"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social_insta.jpg" /></a> <a href="http://pinterest.com/pin/create/button/?url=<?php echo urlencode($current_url) ?>&media=<?php echo $img_obj[0]; ?>&description=<?php echo str_replace(" ", "+", get_the_title(get_the_ID())); ?>" class="pinterest"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social_pin.jpg" /></a></div>
 
               <?php //if(!empty($post_categories) || is_user_logged_in()) { ?>
 							<?php
@@ -222,7 +224,40 @@
     
     
 	</section><!-- /#content -->
-  <?php inf_browse_by(); ?>
+  <?php #inf_browse_by(); ?>
+
+  <?php
+  ini_set('display_errors', 1);
+  // The interviews page displays the latest interview for now
+  $args = array(
+    'numberposts' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'post_type' => 'inf-interview',
+    'post_status' => 'publish'
+  );
+  $interviews = get_posts($args);
+  #print_r($interviews);
+  $key = 0;
+  ?>
+  <div class="more_interviews">
+	<div class="more_interviews_wrapper">
+	<?php foreach ($interviews as $_interview):
+		#print_r($_interview); die();
+		$image_id = get_post_thumbnail_id($_interview->ID);
+		$thumbnail = wp_get_attachment_image_src($image_id, 'medium');
+	?>
+			<div class="more_int_block">
+                <a href="<?php echo $_interview->post_link; ?>">
+                <img src="<?php echo $thumbnail[0]; ?>" alt="<?php echo $_interview->post_title; ?>" width="100%" />
+				<h3><?php echo $_interview->post_title; ?></h3>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/btn_viewmore.jpg" />
+                </a>
+            </div>
+	<?php
+	endforeach; ?>
+		</div>
+  </div>
 <?php get_footer(); ?>
 
 <?php
