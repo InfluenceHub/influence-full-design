c3b0f20b830f60865d18bc499107d27a4a066afb
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-01 12:02:48 -0700

    more testing

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -42,7 +42,7 @@
               ?>
               <div class="inner" style="width: 430px; height:600px; margin-right:24px; background:#d6f7ee">
 								<ul class="slides"><?php } ?>
-								<li><a href="<?php echo $img_obj[1]; ?>" class="colorbox">
+								<li><a href="<?php echo $img_obj[2]; ?>" class="colorbox">
 									<?php the_post_thumbnail('inf_featured_theme', array('class' => 'inf_featured_theme')); ?>
 								</a></li>
                 <?php inf_theme_slider_theme(); ?>
