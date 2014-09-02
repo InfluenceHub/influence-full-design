88048e4e1e5f7e7e6e32969ce57121988a311337
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-01 21:57:29 -0700

    more interviews off featured themes

--- functions_two.php
+++ functions_two.php
@@ -420,7 +420,8 @@ return null;
           </div>
         </div>
         <div class="shell">
-         <h2 class="no-mobile"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/styleset.png" /></h2><br class="no-mobile" />
+                 <h2 class="no-mobile column-two"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/styleset.png" /></h2><br class="no-mobile" />
+
          <ul>
             <?php
               $latest_theme = inf_get_latest_theme();
@@ -431,6 +432,7 @@ return null;
                 ?>
                 <?php if($k <= 3): // Show the first 4 posts as two column posts ?>
                   <li class="column-two home-feed-post">
+
                         <div class="home-feed-post-img-wrap">
                         <a href="<?php the_permalink(); ?>">
                           <?php
