b0f7fe1d4661dcae4e44942b0275715e385c32e3
Cameron Banowsky <cameron@code.cd>
2014-09-02 13:42:19 -0700

    change font size

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -246,7 +246,7 @@
 		#print_r($_interview); die();
 		$image_id = get_post_thumbnail_id($_interview->ID);
 		$thumbnail = wp_get_attachment_image_src($image_id, 'medium');
-    $link = get_permalink($interview->ID);
+    $link = get_permalink($interviews->ID);
 
 	?>
 			<div class="more_int_block">
