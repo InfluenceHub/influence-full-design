5f5545051aa4539e3498e32c9e3f597c03b41564
Cameron Banowsky <cameron@code.cd>
2014-09-02 16:42:52 -0700

    post captions

--- options/custom-fields.php
+++ options/custom-fields.php
@@ -158,10 +158,9 @@ Carbon_Container::factory('custom_fields', __('Images settings', 'inf'))
 Carbon_Container::factory('custom_fields', __('Additional Featured Images', 'inf'))
 	->show_on_post_type(array('post', 'inf_theme'))
 	->add_fields(array(
-		Carbon_Field::factory('complex', 'inf_featured_images', 'Images')
+		Carbon_Field::factory('complex', 'inf_featured_images', 'Images'),
 			->add_fields(array(
-				Carbon_Field::factory('attachment', 'inf_featured_image', 'Image')
-			))
+				Carbon_Field::factory('attachment', 'inf_featured_image', 'Image'),
 		Carbon_Field::factory('complex', 'inf_captions', 'Captions')
 			->add_fields(array(
 				Carbon_Field::factory('textarea', 'inf_caption', 'Caption')
