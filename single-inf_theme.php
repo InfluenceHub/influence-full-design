d5ba49d5be46233d07016914eb358d69578014f8
Cameron Banowsky <cameron@code.cd>
2014-09-03 15:41:37 -0700

    fix sitemap

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -267,14 +267,14 @@
 function inf_theme_slider_theme() {
 	global $post;
 	$images = carbon_get_post_meta($post->ID, 'inf_featured_image', 'complex');
-	$captions = carbon_get_post_meta($post->ID 'inf_captions', 'complex');
 	foreach($images as $image) {
 		$image_full  = wp_get_attachment_image_src($image[inf_featured_image], 'full');
 		$image_small = wp_get_attachment_image_src($image[inf_featured_image], 'inf_featured_theme');
+		$captions = carbon_get_post_meta($post->ID 'inf_caption', 'complex');
 		?>
 		<li class="slide-theme"><a href="<?php echo $image_full[0]; ?>" class="colorbox">
 			<img src="<?php echo $image_small[0]; ?>" class="inf_single_image" />
-			<?php print_r($caption[0]); ?>
+			<?php print_r($captions[0]); ?>
 		</a></li>
 		<?php
 	}
