dd3a20148c7a00f0d0ad4265b30ae7fd7719ba9b
Cameron Banowsky <cameron@code.cd>
2014-09-03 17:57:33 -0700

    fix sitemap

--- header.php
+++ header.php
@@ -41,26 +41,19 @@ $detect = new Mobile_Detect;
 
 
 <script type='text/javascript'>
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
 (function() {
-var gads = document.createElement('script');
-gads.async = true;
-gads.type = 'text/javascript';
 var useSSL = 'https:' == document.location.protocol;
-gads.src = (useSSL ? 'https:' : 'http:') + 
+var src = (useSSL ? 'https:' : 'http:') +
 '//www.googletagservices.com/tag/js/gpt.js';
-var node = document.getElementsByTagName('script')[0];
-node.parentNode.insertBefore(gads, node);
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
 })();
 </script>
 
 <script type='text/javascript'>
-googletag.cmd.push(function() {
-googletag.defineSlot('/61413193/BCBG_HEADER', [728, 90], 'div-gpt-ad-1409791364321-0').addService(googletag.pubads());
+googletag.defineSlot('/61413193/BCBG_HEADER', [728, 90], 'div-gpt-ad-1409792194307-0').addService(googletag.pubads());
+googletag.pubads().enableSyncRendering();
 googletag.pubads().enableSingleRequest();
 googletag.enableServices();
-});
 </script>
   </head>
 
