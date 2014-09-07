00f6c4c910c0aa16eaebb12f7d0d36f3faff63ee
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-06 17:17:26 -0700

    ad adjustments

--- functions_two.php
+++ functions_two.php
@@ -670,6 +670,12 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1409963968603-0');
             <a href="<?php echo home_url(); ?>/my-influence" class="subscribe-img-home"></a>
             <div class="line" style="margin-top: 7px; padding-top: 7px;"></div>
             <?php inf_item_of_the_day(); ?>
+<!-- 300x500BCBG -->
+<div id='div-gpt-ad-1409963968603-0' style='width:300px; height:600px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1409963968603-0'); });
+</script>
+</div>
             <div class="line" style="margin-top: 7px; padding-top: 7px;"></div>
             <?php inf_qa(); ?>
             <!-- <div class="hash-influence"></div> -->
--- single.php
+++ single.php
@@ -10,7 +10,7 @@
 	<section id="content">
     <div class="ad_banner no-mobile">
 <!-- BCBG_HEADER -->
-<div id='div-gpt-ad-1409963968603-1' style='width:971px; height:120px; margin:100px auto 15px;'>
+<div id='div-gpt-ad-1409963968603-1' style='width:971px; height:120px; margin: 0 auto;'>
 <script type='text/javascript'>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1409963968603-1'); });
 </script>
