f03969410e622f7bd01408ec28775d27b481767a
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-07 19:10:11 -0700

    chrome php

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -282,10 +282,10 @@ function inf_theme_slider_theme() {
   global $post;
   $images = carbon_get_post_meta($post->ID, 'inf_featured_image', 'complex');
   $captions = carbon_get_post_meta($post->ID, 'inf_caption' ,  'complex');
-  $caption = get_post_meta($captions[0]);
   foreach($images as $image) {
     $image_full  = wp_get_attachment_image_src($image[inf_featured_image], 'full');
     $image_small = wp_get_attachment_image_src($image[inf_featured_image], 'inf_featured_theme');
+    $caption = carbon_get_post_meta($image[inf_caption]);
     ?>
     <li class="slide-theme">
       <img src="<?php echo $image_small[0]; ?>" class="inf_single_image" />
