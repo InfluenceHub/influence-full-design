e018f7992d49d6ddd457d9c0f8abe69133dc59f8
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-07 18:49:15 -0700

    chrome php

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -288,7 +288,7 @@ function inf_theme_slider_theme() {
     ?>
     <li class="slide-theme">
       <img src="<?php echo $image_small[0]; ?>" class="inf_single_image" />
-      <pre><?php echo print_r($captions); ?></pre>
+      <pre><?php echo print_r($captions[0]); ?></pre>
 		</li>
      <?php
     }   
