72307849c9658c364731e6681554454317c578f9
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-07 18:35:52 -0700

    chrome php

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -286,12 +286,14 @@ function inf_theme_slider_theme() {
     $image_full  = wp_get_attachment_image_src($image[inf_featured_image], 'full');
     $image_small = wp_get_attachment_image_src($image[inf_featured_image], 'inf_featured_theme');
     $caption = $captions[0];
+    foreach ($captions as $caption) {
     ?>
     <li class="slide-theme">
       <img src="<?php echo $image_small[0]; ?>" class="inf_single_image" />
       <pre><?php echo print_r($caption); ?></pre>
 		</li>
      <?php
+     }
     }   
   }
 ?>
