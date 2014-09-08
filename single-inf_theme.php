dfbf023f8edd5e7f57e5af7f4da3e6da6f1f3ef4
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-07 18:09:54 -0700

    chrome php

--- single-inf_theme.php
+++ single-inf_theme.php
@@ -290,7 +290,7 @@ function inf_theme_slider_theme() {
     ?>
     <li class="slide-theme">
       <img src="<?php echo $image_small[0]; ?>" class="inf_single_image" />
-      <pre><?php echo print_r($caption[0]); ?></pre>
+      <pre><?php echo print_r($caption[$captions - 1]); ?></pre>
 		</li>
     <?php
    }
