a53dcad5eedddc8808985cdb1b757200fa081c98
Cameron Banowsky <cameronbanowsky@Camerons-MacBook-Pro.local>
2014-09-12 13:47:51 -0700

    homepage takeover

--- header.php
+++ header.php
@@ -55,7 +55,7 @@ node.parentNode.insertBefore(gads, node);
 </script>
 <script type='text/javascript'>
 googletag.cmd.push(function() {
-googletag.defineSlot('/61413193/300x500BCBG', [300, 600], 'div-gpt-ad-1409963968603-0').addService(googletag.pubads());
+googletag.defineSlot('/61413193/300x500BCBG', [300, 600], 'div-gpt-ad-1410554390932-0').addService(googletag.pubads());
 googletag.defineSlot('/61413193/BCBG_HEADER', [970, 120], 'div-gpt-ad-1410551574695-0').addService(googletag.pubads());
 googletag.defineSlot('/61413193/Right_Skin', [120, 1000], 'div-gpt-ad-1409963968603-2').addService(googletag.pubads());
 googletag.defineSlot('/61413193/Skin_Sides', [120, 1000], 'div-gpt-ad-1409963968603-3').addService(googletag.pubads());
