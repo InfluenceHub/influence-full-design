5c01121c1497f0fa437d43f6f1048de2aea72a8d
Cameron Banowsky <cameron@code.cd>
2014-09-15 13:23:02 -0700

    350 x 200 change up

--- loop.php
+++ loop.php
@@ -97,10 +97,11 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410302527331-0');
         ?>
              <div class="archive-col">
 <!-- 300x250_ROS -->
-             <div class="ad-300-250">
-          <a href="http://click.linksynergy.com/fs-bin/click?id=pwlaa2*cgnI&offerid=276224.10013358&subid=0&type=4">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/nordstrom1.jpg" />
-          </a>
+<div id='div-gpt-ad-1410811923506-0' style='width:300px; height:250px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410811923506-0'); });
+</script>
+</div>
                 <!-- BEGIN IFRAME TAG - theinfluence 300x250 < - DO NOT MODIFY -->
                 <!--<IFRAME SRC="http://ib.adnxs.com/tt?id=2411077&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="250"></IFRAME>-->
                 <!-- END TAG -->
@@ -110,11 +111,10 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410302527331-0');
       if ($i == 5 and $profile_flag == true) {
         ?>
 <!-- 300x250_ROS -->
-<div id='div-gpt-ad-1410811923506-0' style='width:300px; height:250px;'>
-<script type='text/javascript'>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410811923506-0'); });
-</script>
-</div>
+             <div class="ad-300-250">
+          <a href="http://click.linksynergy.com/fs-bin/click?id=pwlaa2*cgnI&offerid=276224.10013358&subid=0&type=4">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/nordstrom1.jpg" />
+          </a>
               <!-- BEGIN IFRAME TAG - theinfluence 300x250 < - DO NOT MODIFY -->
                 <!--<IFRAME SRC="http://ib.adnxs.com/tt?id=2411077&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="250"></IFRAME>-->
                 <!-- END TAG -->
