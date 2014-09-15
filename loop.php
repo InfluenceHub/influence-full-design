a5d6679d692a8a2f93156718ae40beea9202ae0d
Cameron Banowsky <cameron@code.cd>
2014-09-15 13:17:33 -0700

    350 x 200 change up

--- loop.php
+++ loop.php
@@ -103,7 +103,7 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410302527331-0');
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410811923506-0'); });
 </script>
 </div>
-</div>
+
                 <!-- BEGIN IFRAME TAG - theinfluence 300x250 < - DO NOT MODIFY -->
                 <!--<IFRAME SRC="http://ib.adnxs.com/tt?id=2411077&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="250"></IFRAME>-->
                 <!-- END TAG -->
