a20067d46878ae2d1f2374fa4b0e4851dadab8b6
Cameron Banowsky <cameron@code.cd>
2014-09-15 13:07:36 -0700

    350 x 200 change up

--- loop.php
+++ loop.php
@@ -95,11 +95,11 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410302527331-0');
       }
       if ($i == 3 and $profile_flag != true) {
         ?>
-           <div class="archive-col">
-             <div class="ad-300-250">
-          <a href="http://click.linksynergy.com/fs-bin/click?id=pwlaa2*cgnI&offerid=276224.10013358&subid=0&type=4">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/nordstrom1.jpg" />
-          </a>
+<div id='div-gpt-ad-1410397639329-0' style='width:300px; height:250px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410397639329-0'); });
+</script>
+</div>
                 <!-- BEGIN IFRAME TAG - theinfluence 300x250 < - DO NOT MODIFY -->
                 <!--<IFRAME SRC="http://ib.adnxs.com/tt?id=2411077&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="250"></IFRAME>-->
                 <!-- END TAG -->
@@ -155,14 +155,11 @@ googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410397639329-0');
       </div>
       <?php if ($i == 9 and $profile_flag != true) { ?>
         <div class="archive-col">
-          <div class="ad-300-250">
-          <a href="http://click.linksynergy.com/fs-bin/click?id=pwlaa2*cgnI&offerid=338823.10000514&subid=0&type=4">
-            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/asos1.gif" />
-          </a>
-            <!-- BEGIN IFRAME TAG - theinfluence 300x250 < - DO NOT MODIFY -->
-            <!--<IFRAME SRC="http://ib.adnxs.com/tt?id=2411077&cb=[CACHEBUSTER]&referrer=[REFERRER_URL]&pubclickenc=%5BINSERT_CLICK_TAG%5D" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="300" HEIGHT="250"></IFRAME>-->
-            <!-- END TAG -->
-          </div>
+<div id='div-gpt-ad-1410397639329-0' style='width:300px; height:250px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1410397639329-0'); });
+</script>
+</div>
         </div>
       <?php } ?>
 		<?php endwhile; ?>
