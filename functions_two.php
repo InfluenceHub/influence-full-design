bec4a361ac9f690b4fa9b43d466f0b91dd251ec3
Cameron Banowsky <cameron@hack.cd>
2014-09-30 16:40:22 -0700

    spacing

--- functions_two.php
+++ functions_two.php
@@ -121,7 +121,7 @@ function inf_feature() {
   $feature = get_post($args);
   $feature_link = get_post_meta($feature->ID, 'feature_url', true);
   $main_boxID =  get_post_meta($feature->ID, 'main_image');
-  $main_image = get_post($main_boxID[0]->guid);
+  $main_image = get_post_meta($main_boxID[0]->guid);
   
 ?>
 
@@ -130,7 +130,7 @@ function inf_feature() {
       <a href="<?php echo $feature_link ?>">
         <img src="<?php echo $main_image ?>" />
       </a>
-</div>
+  </div>
 
 <?php 
 }
